2720b52be3b642a42cbfe3257d72dbad69b951a4
freddygreve <freddy@fred-dienste.de>
2017-06-22 09:07:14 +0200

    Create ajax-hotels.php

---
+++ Aufgabe 6/Aufgabe 6.2/ajax-hotels.php
@@ -0,0 +1,15 @@
+<?php
+    $suche = utf8_decode($_REQUEST["q"]);
+    $mysqli = new mysqli("dbs.hs-furtwangen.de", "TestUser", "TestGIS", "prof_taube");
+    $sql = "SELECT * FROM flughafen WHERE `stadtname` = '$suche' LIMIT 5";
+    $result = $mysqli->query($sql);
+    while($row = mysqli_fetch_object($result)) {
+        $flughafen = $row->idflughafen;
+    }
+
+    $sql = "SELECT * FROM hotel WHERE `idflughafen` = '$flughafen'";
+    $result = $mysqli->query($sql);
+    while($row = mysqli_fetch_object($result)) {
+        echo '<option value="1">'.utf8_encode($row->hotelname).'</option>';
+    }
+?>
