ddff0200342692578a0850a945c11a09a912b76a
Freddy Greve <freddy@fred-dienste.de>
2017-06-27 18:07:27 +0200

    Delete getZielflughafen.php

--- Aufgabe 6/Aufgabe 6.2/getZielflughafen.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $suche = utf8_decode($_REQUEST["city"]);
-    $mysqli = new mysqli("dbs.hs-furtwangen.de", "TestUser", "TestGIS", "prof_taube");
-    $sql = "SELECT * FROM hotel WHERE `idflughafen` = '$suche'";
-    $result = $mysqli->query($sql);
-    while($row = mysqli_fetch_object($result)) {
-        echo '<option value="'.$row->idhotel.'">'.utf8_encode($row->hotelname).'</option>';
-    }
-?>
